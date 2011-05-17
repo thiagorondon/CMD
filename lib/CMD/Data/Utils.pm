6a982f281e5c19733ce06d156455ce28436fa277
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-05-17 21:09:19 +0000

    Adicionando rotina para facilitar encontrar o node root.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/CMD/Data/Utils.pm
@@ -0,0 +1,28 @@
+
+package CMD::Data::Utils;
+
+use strict;
+use warnings;
+use vars qw(@ISA @EXPORT_OK $VERSION @EXPORT_FAIL);
+require Exporter;
+
+@ISA       = qw(Exporter);
+@EXPORT_OK = qw(get_root_by_nodeid);
+
+$VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+sub get_root_by_nodeid {
+    my $node = shift;
+    my $root = $node;
+    my $point = $node;
+    while ($point) {
+        my @parent = $point->parents();
+        $root = $parent[0] if scalar @parent;
+        $point = $point->parent;
+    }
+    return $root;
+}
+
+1;
+
